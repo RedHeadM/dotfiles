c6ea9625429dcb448a4398ca749bb95be66fd8ee
RedHeadM <merklingermarkus@gmail.com>
2018-11-08 18:39:39 +0100

    install neo vim

---
+++ install_ubuntu/nvim_tools.sh
@@ -0,0 +1,13 @@
+
+
+# from http://nvim.fisadev.com/
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+
+# already installed: python-dev python-pip python3-dev python3-pip git
+sudo apt-get instagll curl vim exuberant-ctags  ack-grep
+sudo pip install --user neovim pep8 flake8 pyflakes pylint isort
+sudo pip3 install --user neovim pep8 flake8 pyflakes pylint isort
+# onivim py language support
+pip install --user python-language-server
