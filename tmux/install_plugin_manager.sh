f7503175f20ddc32f403f30039bf6bc325c4b25f
RedHeadM <merklingermarkus@gmail.com>
2021-05-01 23:59:37 +0200

    clean up, rm not needed files

--- tmux/install_plugin_manager.sh
+++
@@ -1,5 +0,0 @@
-# Tmux Plugin Manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-#  for tmux-yank puligin: Copy to the system clipboard in tmux
-sudo apt-get -y install xclip
